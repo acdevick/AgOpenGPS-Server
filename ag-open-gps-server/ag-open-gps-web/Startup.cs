fa4f9b78da97574e1b1ea89c0b8573c2f16e6b03
Tate <buttontate@gmail.com>
2019-11-16 00:50:53 -0600

    startup items

--- ag-open-gps-server/ag-open-gps-web/Startup.cs
+++ ag-open-gps-server/ag-open-gps-web/Startup.cs
@@ -55,8 +55,7 @@ namespace ag_open_gps_web
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-                app.UseHttpsRedirection();
+                // app.UseHsts();
             }
             
             app.Use(async (context, next) =>
